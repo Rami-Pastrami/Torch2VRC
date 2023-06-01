89dd33c7c29ac1dbc1a217e6a993104e11e2fb79
Rami-Pastrami <rami@ramipastrami.engineering>
2023-05-31 20:38:59 -0400

    Added Network Def

--- Resources/NetClasses.py
+++ Resources/NetClasses.py
@@ -1,8 +1,46 @@
 import numpy as np
 import torch as pt
+from torch import nn
+
+
+class NetworkDef(nn.Module):
+
+
+
+    def __init__(self, layerSizes: list[list], layerNames: list, layerTypes: list):
+
+        super().__init__()
+
+        if len(layerSizes) != len(layerNames):
+            raise Exception("Invalid number of neuron sets given number of layers")
+        if len(layerTypes) != len(layerTypes):
+            raise Exception("Invalid number of layer names given number of layer types")
+
+        self.layers: dict = {}
+
+        for i in range(len(layerNames)):
+            # define layer by name using the layers type, with its number input and output neurons respectfully
+            self.layers[layerNames[i]] = layerTypes[i](layerSizes[i][0], layerSizes[i][1])
+
+
+    def Forward(self, input: pt.Tensor) -> pt.Tensor:
+
+
+
+
+
+
+
+
+
 
 
 class TrainingSet():
+    '''
+    Holds Training and Testing data for a specific neural network model
+    '''
+
+
 
     def __init__(self, trainingSets: dict, testingSets: list):
         '''
