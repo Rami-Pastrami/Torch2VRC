c944b1b51ba6498a5317bfbc64bad0888310f130
Rami-Pastrami <rami@ramipastrami.engineering>
2023-06-15 03:19:44 -0400

    Redo RGB File with NN and training

--- RGB_Load_Train_Export.py
+++ RGB_Load_Train_Export.py
@@ -2,48 +2,31 @@ from torch import nn
 import torch as pt
 
 from Torch2VRC import Loading
-from Torch2VRC import NetClasses
-
-# import data
-importedLog: dict = Loading.LoadLogFileRaw("RGB_Demo_Logs.log")
-
-# Generate testing sets
-answerTable, answerCounts = Loading.GenerateTestingData(importedLog)
-
-
-
-RGB_Net = NetClasses.NetworkDef(3, 10, 5)
-
-
-# separate into training and testing sets
-
-testing = RGB_Net.GenerateClassifierTestingTensor(answerCounts)
-
-# Generate Training Sets
-trainingSet = RGB_Net.GenerateClassifierTrainingTensors(importedLog, [["red", "green", "blue", "magenta", "yellow"]])
-
-# train model
-
-NetClasses.Train(RGB_Net, trainingSet[0], testing)
-
-
-
-
-
+from Torch2VRC import NNWorker
 
 # Actual Network Model
-
 class RGB_NN(nn.Module):
-
     def __init__(self, numNeuronsHidden: int, numInputs: int = 3, numAnswers: int = 5):
         super().__init__()
         self.innerConnections = nn.Linear(numInputs, numNeuronsHidden)
         self.outerConnections = nn.Linear(numNeuronsHidden, numAnswers)
 
-    def forward(self, input: pt.Tensor):
-        hiddenLayer: pt.Tensor = pt.tanh(self.innerConnections(input))  # add activation function
+    def forward(self, *inputs):
+        hiddenLayer: pt.Tensor = pt.tanh(self.innerConnections(inputs[0]))  # add activation function
         return self.outerConnections(hiddenLayer)
 
+
+# import data
+importedLog: dict = Loading.LoadLogFileRaw("RGB_Demo_Logs.log")
+
+# Init NN Builder
+RGB_Builder = NNWorker.NNDataBuilder(importedLog, [["red", "green", "blue", "magenta", "yellow"]])
+
+# Init NN Network
+RGB_Net = RGB_NN(10)
+
+# Train
+RGB_Net = RGB_Builder.Train(RGB_Net)
 
 
 print("convinient breakpoint")
