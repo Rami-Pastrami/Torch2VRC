4c42c9ce0d86e8faa76f9dac2c9072a029153e35
Rami-Pastrami <rami@ramipastrami.engineering>
2023-06-15 01:33:59 -0400

    Make NN itself a project specific definition

--- RGB_Load_Train_Export.py
+++ RGB_Load_Train_Export.py
@@ -1,17 +1,22 @@
+from torch import nn
+import torch as pt
+
 from Torch2VRC import Loading
 from Torch2VRC import NetClasses
 
 # import data
 importedLog: dict = Loading.LoadLogFileRaw("RGB_Demo_Logs.log")
-# separate data into separate channels by color, and prep them for importing into PyTorch
+
+# Generate testing sets
+answerTable, answerCounts = Loading.GenerateTestingData(importedLog)
+
+
 
 RGB_Net = NetClasses.NetworkDef(3, 10, 5)
 
 
 # separate into training and testing sets
 
-# Generate testing sets
-answerTable, answerCounts = Loading.GenerateTestingData(importedLog)
 testing = RGB_Net.GenerateClassifierTestingTensor(answerCounts)
 
 # Generate Training Sets
@@ -20,6 +25,25 @@ trainingSet = RGB_Net.GenerateClassifierTrainingTensors(importedLog, [["red", "g
 # train model
 
 NetClasses.Train(RGB_Net, trainingSet[0], testing)
+
+
+
+
+
+
+# Actual Network Model
+
+class RGB_NN(nn.Module):
+
+    def __init__(self, numNeuronsHidden: int, numInputs: int = 3, numAnswers: int = 5):
+        super().__init__()
+        self.innerConnections = nn.Linear(numInputs, numNeuronsHidden)
+        self.outerConnections = nn.Linear(numNeuronsHidden, numAnswers)
+
+    def forward(self, input: pt.Tensor):
+        hiddenLayer: pt.Tensor = pt.tanh(self.innerConnections(input))  # add activation function
+        return self.outerConnections(hiddenLayer)
+
 
 
 print("convinient breakpoint")
