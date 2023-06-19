71c0cc70737d87bfa17f64dbabd02fae08cb6f59
Rami-Pastrami <rami@ramipastrami.engineering>
2023-06-19 01:52:05 -0400

    Started NNRect

--- Torch2VRC/ShaderExport.py
+++ Torch2VRC/ShaderExport.py
@@ -1,4 +1,27 @@
-#TODO
+import numpy as np
+
+class NNRect():
+
+    width: int
+    height: int
+    centerPos: np.ndarray
+    corners: list[np.ndarray]
+    isRotated: bool
+    originalData: np.ndarray
+
+
+
+    def __init__(self, layerData: np.ndarray, XYOffset: np.ndarray):
+
+        self.height, self.width = layerData.shape
+        self.centerPos = np.asarray([self.width / 2.0, self.height / 2.0]) + XYOffset  # initial
+        self.originalData = layerData
+
+    def get_area(self) -> int:
+        return self.width * self.height
+
+    area: int = property(get_area)
+
 
 
 # Calc minimum size CRT block per component
