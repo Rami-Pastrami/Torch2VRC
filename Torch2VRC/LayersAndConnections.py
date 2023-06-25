156804762909e077996470d2cc562f5a264d4251
Rami-Pastrami <rami@ramipastrami.engineering>
2023-06-25 04:33:57 -0400

    Add Float array / 1D layers, refactor Linear Connections

--- Torch2VRC/LayersAndConnections.py
+++ Torch2VRC/LayersAndConnections.py
@@ -1,4 +1,4 @@
-from Torch2VRC.ImageExport import ExportNPArrayAsPNG
+from Torch2VRC.ImageExport import ExportNPArrayAsPNGAndGetNormalizer
 import numpy as np
 from pathlib import Path
 
@@ -6,72 +6,81 @@ from pathlib import Path
 
 # No data held in these! just for context reference
 
-class Layer_FloatArray():
-    length: int = None
-    ArrayName: str = None
+class Layer_Base():
     postConnectionNames: list = []
+    layerName: str = None
 
-    def __init__(self, length: int, ArrayName: str, postConnectionNames: list):
-        self.length = length
-        self.postConnectionNames = postConnectionNames
+class Layer_FloatArray(Layer_Base):
+
+    ArrayName: str = None
+    size: int = None
+
+    def __init__(self, size: int, layerName: str, ArrayName: str):
+        self.size = size
+        self.layerName = layerName
 
         nameSlice = ArrayName[0:6]
         if nameSlice != "_Udon_":
             raise Exception("Uniform Float Array Names Must Start With '_Udon_' Exactly!")
         self.ArrayName = ArrayName
 
-class Layer_1D():
+class Layer_1D(Layer_Base):
 
-    length: int = None
-    layerName: str = None
     priorConnectionNames: list = []
-    postConnectionNames: list = []
+    size: int = None
 
-    def __init__(self, length: int, layerName: str, priorConnectionNames: list, postConnectionNames: list):
-        self.length = length
+    def __init__(self, size: int, layerName: str, priorConnectionNames: list):
+
         self.layerName = layerName
         self.priorConnectionNames = priorConnectionNames
-        self.postConnectionNames = postConnectionNames
+        self.size = size
 
 
 #################### Connections ####################
 
+# These hold data for biases / weights
+
 class Connection_Linear():
 
+    inputSize: int = None
+    outputSize: int = None
     connectionName: str = None
     weight: np.ndarray = None
     bias: np.ndarray = None
     combinedWeightBias: np.ndarray = None
-    outputLayerName: str = None
-    inputLayerName: str = None
-    inputDataName: str = None
+    outputNames: list[str] = None
+    inputNames: list[str] = None
+    activation: str = None  # can be none or tanh
 
-    def __init__(self, weight: np.ndarray, bias: np.ndarray, connectionName: str, outputLayerName: str,
-                 inputLayerName: str, inputDataName: str = None, shouldTransposeBias: bool = True):
+    def __init__(self, weight: np.ndarray, bias: np.ndarray, connectionName: str, outputNames: list[str],
+                 inputNames: list[str], activation: str, inputSize: int, outputSize: int):
 
+        self.inputSize = inputSize
+        self.outputSize = outputSize
         self.weight = weight
-        self.outputLayerName = outputLayerName
-        self.inputLayerName = inputLayerName
-        self.inputDataName = inputDataName
+        self.outputNames = outputNames
+        self.inputNames = inputNames
         self.connectionName = connectionName
+        self.bias = bias
 
-        if shouldTransposeBias:
-            self.bias = bias.transpose()
-        else:
-            self.bias = bias
+        if (activation != "none") or (activation != "tanh"):
+            raise Exception(f"Unknown activation type {activation}!")
 
-    def ExportConnectionData(self, folderPath: str) -> dict:
+        self.activation = activation
+
+
+    def ExportConnectionData(self, connectionFolderPath: Path) -> dict:
         '''
         Exports Linear Weights and Bias as PNGs into the specified folder
         :param folderPath: Folder export path ending with /
         :return: dict containing normalizations for weights and bias
         '''
         output: dict = {}
-        output["weights"] = ExportNPArrayAsPNG(self.weight, folderPath + "WEIGHTS.png")
-        output["bias"] = ExportNPArrayAsPNG(self.bias, folderPath + "BIAS.png")
+        output["weights"] = ExportNPArrayAsPNGAndGetNormalizer(self.weight, connectionFolderPath / "WEIGHTS.png")
+        output["bias"] = ExportNPArrayAsPNGAndGetNormalizer(self.bias, connectionFolderPath / "BIAS.png")
         return output
 
-    def ExportFull(self, folderPath: str):
+    def ExportFull(self, folderPath: Path):
         normalizations = self.ExportConnectionData(folderPath)
         # TODO shader data loader export
         # TODO CRT generation
