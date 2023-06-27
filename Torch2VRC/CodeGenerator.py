26a04fb9a7ea586c8c1fb458e80542f1fa7d3ac9
Rami-Pastrami <rami@ramipastrami.engineering>
2023-06-27 03:10:06 -0400

    Add Code Generator and Editor Generation Function

---
+++ Torch2VRC/CodeGenerator.py
@@ -0,0 +1,13 @@
+import chevron as ch
+from pathlib import Path
+
+def GenerateEditorNetworkImporter(unityNetworkFolderPath: Path, networkName: str):
+
+    mustachePath: Path = Path.cwd() / "Torch2VRC/Resources/Editor_ImportNetwork.cs.moustache"
+    filePath: Path = unityNetworkFolderPath / "Editor_ImportNetwork.cs"
+    with open(mustachePath, 'r') as template:
+        generatedText = ch.render(template, {"NetworkName": networkName})
+        Path.touch(filePath)
+        newFile = open(filePath, 'w')
+        _ = newFile.write(generatedText)
+        newFile.close()
