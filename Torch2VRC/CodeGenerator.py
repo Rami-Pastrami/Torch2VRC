a20d5f2274085a70e76f6acf963bf70217354373
Rami-Pastrami <rami@ramipastrami.engineering>
2023-06-28 03:19:46 -0400

    Prevent overwriting active unity editor script file

--- Torch2VRC/CodeGenerator.py
+++ Torch2VRC/CodeGenerator.py
@@ -5,6 +5,7 @@ def GenerateEditorNetworkImporter(unityNetworkFolderPath: Path, networkName: str
 
     mustachePath: Path = Path.cwd() / "Torch2VRC/Resources/Editor_ImportNetwork.cs.moustache"
     filePath: Path = unityNetworkFolderPath / "Editor_ImportNetwork.cs"
+    if filePath.exists(): return
     with open(mustachePath, 'r') as template:
         generatedText = ch.render(template, {"NetworkName": networkName})
 
