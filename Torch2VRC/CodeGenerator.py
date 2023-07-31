2921e162734ff2f19551dd7c547f09bef6d0235e
Rami-Pastrami <rami@ramipastrami.engineering>
2023-07-31 01:28:02 -0400

    Fursther cleanup

--- Torch2VRC/CodeGenerator.py
+++ Torch2VRC/CodeGenerator.py
@@ -5,7 +5,7 @@ def GenerateEditorNetworkImporter(unityNetworkFolderPath: Path, networkName: str
 
     mustachePath: Path = Path.cwd() / "Torch2VRC/Resources/Editor_ImportNetwork.cs.moustache"
     filePath: Path = unityNetworkFolderPath / "Editor_ImportNetwork.cs"
-    if filePath.exists(): return
+    if filePath.exists(): return  # this file is not dynamic, do don't override if already exists
     with open(mustachePath, 'r') as template:
         generatedText = ch.render(template, {"NetworkName": networkName})
 
@@ -19,24 +19,40 @@ def GenerateEditorNetworkImporter(unityNetworkFolderPath: Path, networkName: str
 
 def GenerateNetworkShaders(unityNetworkFolderPath: Path, networkName: str):
 
+    moustachePath: Path = Path.cwd() / "Torch2VRC/Resources/LinearLayer.moustache"
 
     def GenerateNoActivation() -> str:
         return ""  # This is just here for code completeness
 
     def GenerateActivationStr_Tanh(inputName: str = "output") -> str:
-        return "output = Activation_Tanh(" + inputName + ")"
+        return f"output = Activation_Tanh({{inputName}})"
 
-
+    "sampler2D _inputTex;"
 
     def GenerateLinearLayerCode(inputConnectionLength: int, outputConnectionLength: int, activationStr: str,
                                 isInputArray: bool = False):
+        '''
+        Generates a Shader / CRT combo running a Linear Layer of the neural network
+        :param inputConnectionLength: int - number of input neurons
+        :param outputConnectionLength: int = number of output neurons
+        :param activationStr: Type of activation function (or lack thereof)
+        :param isInputArray: bool - if the input is not another CRT but rather an
+        :return:
+        '''
 
         # Used mainly in the start of a layer sequence, to get input data for the layer from a uniform float array
         def GetInputFromArray(udonArrayName: str, indexName: str = "weightX") -> str:
-            return udonArrayName + "[" + indexName + "]"
+            return f"{{udonArrayName}}[{{indexName}}]"
 
+        # Used whenever the input is another CRT
         def GetInputFromTex(texName: str = "_texInput") -> str:
-            return "tex2D(" + texName + ", float2(IN.localTexcoord.x, 0.5))"
+            return f"tex2D({{texName}}, float2(IN.localTexcoord.x, 0.5))"
+
+        
+
+
+
+
 
 
 
