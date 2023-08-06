49d8aeb9c4d37e78486f04948abaf17a3980ec00
Rami-Pastrami <rami@ramipastrami.engineering>
2023-08-06 02:15:26 -0400

    Create 1D float array layer class

---
+++ Torch2VRC/Layers/LayerFloatArray1D.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from Torch2VRC.Layers.LayerBase import LayerBase
+
+
+class LayerFloatArray1D(LayerBase):
+
+    num_neurons: int
+    array_name: str # name of the uniform array the shader calls, should start with "_Udon_"
+
+    def __init__(self, layer_name: str, num_neurons: int, array_name: str = "_Udon_input"):
+        super().__init__(layer_name)
+        self.num_neurons = num_neurons
+        self.is_CRT = False
+        self.is_float_array = True
+        self.is_read_only = True  # You cannot write to a uniform array
+        if array_name[0:6] != "_Udon_":
+            raise Exception("All Uniform Array Names must start with '_Udon_' due to VRC restrictions!")
+        array_name = array_name
+
+    def generate_unity_file_resources(self, network_root: Path) -> None:
+        # Uniform Arrays do not need anything written to the file system
+        pass
+
