8a337e8f804e54a62b953c5175c6e443cde92975
Rami-Pastrami <rami@ramipastrami.engineering>
2023-08-11 00:06:49 -0400

    Complete Train function

--- Torch2VRC/Trainers/TrainerBase.py
+++ Torch2VRC/Trainers/TrainerBase.py
@@ -3,6 +3,7 @@ import torch as pt
 from torch import nn
 from torch import optim
 
+
 class TrainerBase:
 
     network: pt.nn
@@ -31,9 +32,65 @@ class TrainerBase:
             counts[key] = len(raw_training_data[key])
         return counts
 
-    #def train_network
+    def train_network(self, testing_inputs: list[pt.Tensor], testing_predictions: pt.Tensor,
+                      number_epochs: int = 2000, learning_rate: float = 0.0001, loss_function=nn.MSELoss()) -> None:
+        """
+        Trains self.network of up to 3 input layers of complexity
+        :param testing_inputs: list of inputs (as Tensors), where each element index MUST be in the same input index
+        order for running network model directly
+        :param testing_predictions: the singular expected output tenser expected for the input tensor(s)
+        :param number_epochs: num epochs to train. Defaults to 2000
+        :param learning_rate: Learning rate. Defaults to 0.0001
+        :param loss_function: function to use, defaults to nn.MSELoss()
+        :return:
+        """
+        def _Train1Input(net,  numEpochs: int, optimizer, lossFunction, input1: pt.Tensor):
 
+            for epochI in range(numEpochs):
+                lossI = lossFunction(testing_predictions, net(input1))
+                optimizer.zero_grad()
+                lossI.backward()
+                optimizer.step()
+                print(lossI.item())
+            print("Training Complete!")
+            return net
 
+        def _Train2Input(net, numEpochs: int, optimizer, lossFunction, input1: pt.Tensor, input2: pt.Tensor):
 
+            for epochI in range(numEpochs):
+                lossI = lossFunction(testing_predictions, net(input1, input2))
+                optimizer.zero_grad()
+                lossI.backward()
+                optimizer.step()
+                print(lossI.item())
+            print("Training Complete!")
+            return net
 
+        def _Train3Input(net, numEpochs: int, optimizer, lossFunction, input1: pt.Tensor, input2: pt.Tensor,
+                         input3: pt.Tensor):
 
+            for epochI in range(numEpochs):
+                lossI = lossFunction(testing_predictions, net(input1, input2, input3))
+                optimizer.zero_grad()
+                lossI.backward()
+                optimizer.step()
+                print(lossI.item())
+            print("Training Complete!")
+            return net
+
+        optimizer = optim.SGD(self.network.parameters(), lr=learning_rate)
+        number_input_layers: int = len(testing_inputs)
+
+        match(number_input_layers):
+            case 1:
+                network = _Train1Input(self.network, number_epochs, optimizer, loss_function, testing_inputs[0])
+            case 2:
+                network = _Train2Input(self.network, number_epochs, optimizer, loss_function, testing_inputs[0],
+                                       testing_inputs[1])
+            case 3:
+                network = _Train3Input(self.network, number_epochs, optimizer, loss_function, testing_inputs[0],
+                                       testing_inputs[1], testing_inputs[2])
+            case _:
+                raise Exception("Unaccounted for number of inputs given!")
+
+        self.network = self.network  # lol
