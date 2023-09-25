3aef71b09b238a6b45d27f358a56cc4cef266d96
Rami-Pastrami <rami@ramipastrami.engineering>
2023-09-24 22:53:52 -0400

    Refactor Json Exporter

--- Torch2VRC/JSONExport.py
+++ Torch2VRC/JSONExport.py
@@ -4,10 +4,10 @@ from pathlib import Path
 # and other resources
 
 
-def generate_CRT(location: Path, width: int, height: int, name: str, dimensions: str = "2D") -> None:
+def generate_CRT_definition(CRT_save_folder: Path, width: int, height: int, name: str, dimensions: str = "2D") -> None:
     """
     Generates a json for Unity to use to generate a CRT of defined dimensions
-    :param location: folder location where the json will be saved
+    :param CRT_save_folder: folder location where the json will be saved
     :param width: CRT width
     :param height: CRT height
     :param name: name of the CRT
@@ -22,40 +22,57 @@ def generate_CRT(location: Path, width: int, height: int, name: str, dimensions:
         "dimensions": dimensions
     }
 
-    JSON_location: Path = location / "CRT.json"
-    with open(JSON_location, "w") as file:
-        json.dump(export_data, file)
+    _write_JSON("CRT", CRT_save_folder, export_data)
 
-def generate_material_texture_load(location: Path, name: str, weightNormalizer: float, biasNormalizer: float = 1.0,
-                                   using_bias: bool = True) -> None:
+def generate_material_connection_definition_with_bias(connection_folder: Path, weight_normalizer: float, bias_normalizer: float) -> None:
     """
     Exports JSON of connection for Unity C# to use to create material for texture (weights and biases) loading
-    :param location: folder location where the json will be saved
-    :param weightNormalizer: Normalizer factor for weights
-    :param biasNormalizer: Normalizer factor for baises
-    :param name: name of the material
-    :param using_bias: if a bias is being used, defaults to True
+    :param connection_folder: folder location where the json will be saved
+    :param weight_normalizer: Normalizer factor for weights
+    :param bias_normalizer: Normalizer factor for baises
     :return: None
     """
-    export_data: dict = {}
-    if using_bias:
-        export_data = {
-            "file_type": "mat",
-            "weightNormalizer": weightNormalizer,
-            "biasNormalizer": biasNormalizer,
-            "name": name,
-            "using_bias": True
-        }
-    else:
-        export_data = {
-            "file_type": "mat",
-            "weightNormalizer": weightNormalizer,
-            "name": name,
-            "using_bias": False
-        }
 
-    JSON_location: Path = location / "texture_load_material.json"
+    export_data: dict = {
+        "file_type": "mat",
+        "weightNormalizer": weight_normalizer,
+        "biasNormalizer": bias_normalizer,
+        "using_bias": True
+    }
+
+    _write_JSON("load_connection_values", connection_folder, export_data)
+
+def generate_material_connection_definition_without_bias(connection_folder: Path, weight_normalizer: float) -> None:
+    """
+    Exports JSON of connection for Unity C# to use to create material for texture (weights) loading
+    :param connection_folder: folder location where the json will be saved
+    :param weight_normalizer: Normalizer factor for weights
+    :return: None
+    """
+
+    export_data: dict = {
+        "file_type": "mat",
+        "weightNormalizer": weight_normalizer,
+        "using_bias": False
+    }
+
+    _write_JSON("load_connection_values", connection_folder, export_data)
+
+def generate_material_connection_layer_definitions(connection_folder: Path, input_layers: list[str], output_layer: str) -> None:
+    """
+
+    :param connection_folder:
+    :param input_layers:
+    :param output_layer:
+    :return:
+    """
+    export_data: dict = {
+        "inputs": input_layers,
+        "output": output_layer
+    }
+    _write_JSON("load_connection_connections", connection_folder, export_data)
+
+def _write_JSON(file_name: str, folder: Path, data: dict) -> None:
+    JSON_location: Path = folder / (file_name + ".json")
     with open(JSON_location, "w") as file:
-        json.dump(export_data, file)
-
-
+        json.dump(data, file)
