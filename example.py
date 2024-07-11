91cb2de88ce5838496b82813f528708d3ed08fcd
Rami-Pastrami <rami@ramipastrami.engineering>
2024-07-11 02:45:29 -0400

    begin work on adding neural network and training data

--- example.py
+++ example.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 import pandas as pd
 import numpy as np
-from  sklearn.preprocessing import LabelEncoder
+from  sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from torch import nn
+import torch as pt
 
 from VRCDataImporter.LogFileImporters import RawLogLine, read_log_file_without_further_formatting, segregate_by_tag_to_dataframe_arrays
 
@@ -18,10 +20,23 @@ data_frame: pd.DataFrame = segregate_by_tag_to_dataframe_arrays(log_line_objects
 
 # Get training / answer sets from the DataFrame
 label_encoder: LabelEncoder = LabelEncoder()
+one_hot_encoder: OneHotEncoder = OneHotEncoder(sparse_output=False)
 data_frame["color_category"] = label_encoder.fit_transform(data_frame["color"])
 XYZ_training_data: np.ndarray = data_frame[["X", "Y", "Z"]].to_numpy()
 color_answer_data: np.ndarray = data_frame["color_category"].to_numpy()
+color_answer_classifier_responses: np.ndarray = one_hot_encoder.fit_transform(color_answer_data.reshape(-1, 1))
+color_answer_lookups: np.ndarray = label_encoder.classes_ # what color the int category represents
 
+## Define torch network
+class RGB_NN(nn.Module):
+    def __init__(self, number_inputs: int, number_neurons_hidden: int, number_answers: int):
+        super().__init__()
+        self.inner_connections = nn.Linear(number_inputs, number_neurons_hidden)
+        self.outer_connections = nn.Linear(number_neurons_hidden, number_answers)
+
+    def forward(self, input_layer: pt.Tensor):
+        hidden_layer: pt.Tensor = pt.tanh(self.inner_connections(input_layer))  # notice how the activation is part of this layer
+        return self.outer_connections(hidden_layer) # but not here
 
 
 print("Program End") # easy breakpoint
