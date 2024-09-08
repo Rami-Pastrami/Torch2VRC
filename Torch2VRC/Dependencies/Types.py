f4ac9bd25f3c9b386b618650264b59b635d85254
Rami-Pastrami <rami@ramipastrami.engineering>
2024-09-08 14:07:19 -0400

    refactor repeated code. Added layer types as a data type for layers

--- Torch2VRC/Dependencies/Types.py
+++ Torch2VRC/Dependencies/Types.py
@@ -1,16 +1,26 @@
 from enum import Enum
 
+
 class ActivationFunction(Enum):
     none = "none"
     tanH = "tanH"
+
 
 class InputType(Enum):
     float_array = "float_array"
     CRT = "CRT"
 
+
 class CRTDataType(Enum):
     RHALF = "RHalf"
+
 
 class WrapMode(Enum):
     CLAMP = "Clamp"
     REPEAT = "Repeat"
+
+
+class LayerType(Enum):
+    INPUT = "Input"
+    HIDDEN = "Hidden"
+    OUTPUT = "Output"
--- Torch2VRC/LayerHelpers.py
+++ Torch2VRC/LayerHelpers.py
@@ -1,5 +1,6 @@
-from Torch2VRC.Dependencies.Types import ActivationFunction, InputType
+from Torch2VRC.Dependencies.Types import InputType, LayerType
 from Torch2VRC.Dependencies.UnityExport import CRT_definition, float_array_definition
+
 
 class AbstractLayerHelper:
     def __init__(self, name: str, x_size: int, y_size: int):
@@ -7,11 +8,15 @@ class AbstractLayerHelper:
         self.x_size: int = x_size
         self.y_size: int = y_size
         self.input_type: InputType = InputType.CRT  # default
+        self.layer_type: LayerType = LayerType.HIDDEN # default
 
     def export_as_JSON_dict(self) -> dict:
         output: dict = {
             "X": self.x_size,
-            "Y": self.y_size
+            "Y": self.y_size,
+            "data_type": self.input_type.value,
+            "layer_type": self.layer_type.value,
+
         }
         return output
 
@@ -26,44 +31,31 @@ class AbstractLayerHelper:
         float_array: float_array_definition = float_array_definition(self.x_size, self.y_size, flatten)
         return float_array.export_as_JSON_dict()
 
+
 class InputLayerHelper(AbstractLayerHelper):
     def __init__(self, name: str, x_size: int, y_size: int, input_type: InputType):
         super().__init__(name, x_size, y_size)
         self.input_type: InputType = input_type
+        self.layer_type: LayerType = LayerType.INPUT
 
-    def export_as_JSON_dict(self) -> dict:
-        output: dict = super().export_as_JSON_dict()
-        output["data_type"] = self.input_type.value
-        output["data_file_name"] = self.input_type.value + ".json"
-        return output
-
-    def export_data_as_JSON_dict(self) -> dict:
+    def export_data_as_JSON_dict(self) -> dict:  # OVERRIDDEN
         if self.input_type == InputType.CRT:
             return self._export_data_as_CRT()
         if self.input_type == InputType.float_array:
             return self._export_data_as_float_data()
 
-class HiddenLayerHelper(AbstractLayerHelper):
-    def __init__(self, name: str, x_size: int, y_size: int, incoming_activation_function: ActivationFunction):
-        super().__init__(name, x_size, y_size)
-        self.incoming_activation_function: ActivationFunction = incoming_activation_function
 
-    def export_as_JSON_dict(self) -> dict:
-        output: dict = super().export_as_JSON_dict()
-        output["incoming_activation_function"] = self.incoming_activation_function.value
-        output["data_type"] = InputType.CRT.value  # Hidden layers will always be CRTs
-        output["data_file_name"] = InputType.CRT.value + ".json"
-        return output
+class HiddenLayerHelper(AbstractLayerHelper):
+    def __init__(self, name: str, x_size: int, y_size: int):
+        super().__init__(name, x_size, y_size)
+        self.layer_type: LayerType = LayerType.HIDDEN
+        self.input_type = InputType.CRT  # will always be CRT
 
 
 class OutputLayerHelper(AbstractLayerHelper):
-    def __init__(self, name: str, x_size: int, y_size: int, incoming_activation_function: ActivationFunction = ActivationFunction.none):
+    def __init__(self, name: str, x_size: int, y_size: int):
         super().__init__(name, x_size, y_size)
-        self.incoming_activation_function: ActivationFunction = incoming_activation_function
+        self.layer_type: LayerType = LayerType.OUTPUT
+        self.input_type = InputType.CRT # will always be CRT
 
-    def export_as_JSON_dict(self) -> dict:
-        output: dict = super().export_as_JSON_dict()
-        output["incoming_activation_function"] = self.incoming_activation_function.value
-        output["data_type"] = InputType.CRT.value  # Output layers will always be CRTs
-        output["data_file_name"] = InputType.CRT.value + ".json"
-        return output
+
